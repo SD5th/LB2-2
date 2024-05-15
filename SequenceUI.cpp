e9e1718d586de4858e401502ae97ce48e1b54725
SD5th <vlaka@ro.ru>
2024-05-15 03:42:16 +0300

    First working UI

--- SequenceUI.cpp
+++ SequenceUI.cpp
@@ -1,11 +1,23 @@
 #include "C:\Main\Kodiki\C++\SimpleConsoleUI\SimpleConsoleUI.h"
-#include "Sequence.cpp"
+#include "Sequence.h"
 #include "SequenceTests.cpp"
 
 ArraySequence<Sequence<int>*> Sequences;
 ArraySequence<string> SequenceNames;
 
-Shell* Head = new Shell("Head Window", Head_Function, new Connector);
+Shell* Head;
+Shell* CreateSequence;
+Shell* ListOfSequences;
+Shell* SequenceOperations;
+Shell* SetOperation;
+Shell* ResizeOperation;
+Shell* InsertOperation;
+Shell* AppendOperation;
+Shell* PrependOperation;
+Shell* Test;
+Shell* Exit;
+
+
 void Head_Function()
 {
     cout << "Hello! This is User Interface for Sequence.h\n"
@@ -46,12 +58,12 @@ void CreateSequence_Function()
         SequenceNames.Append(name);
         Sequences.Append(new ListSequence<int>);
     }
+
     if (Sequences.GetSize() == 1)
     {
-        Head->Get_Connector()->Append_Shell(SequenceOperations);
-        Head->Get_Connector()->Append_Shell(ListOfSequences);
+        Head->Get_Connector()->Insert_Shell(ListOfSequences, 1);
+        Head->Get_Connector()->Insert_Shell(SequenceOperations, 2);
     }    
-
 }
 
 void ListOfSequences_Function()
@@ -71,91 +83,78 @@ void ListOfSequences_Function()
         cout << "\n\n";
     }
 }
-Shell* ListOfSequences = new Shell("List of Sequences", ListOfSequences_Function, new Connector);
 
-
-Shell* SequenceOperations = new Shell("Sequence Operations", nullptr, new Connector);
+//   SequenceOperations = nullptr
 
 void SetOperation_Function()
 {
     cout << "Choose Sequence to Set.\n";
-    cout << "From 1 to " << Sequences.GetSize();
+    cout << "From 1 to " << Sequences.GetSize() << "\n";
     int number, index, item;
     cin >> number;
     cout << "Input index to Set.\n";
     cin >> index;
     cout << "Input item to Set.\n";
     cin >> item;
-    Sequences.Get(number)->Set(index, item);
+    Sequences.Get(number-1)->Set(index, item);
     cout << "Ready!\n";
 }
-Shell* SetOperation_Function = new Shell("Operation Set", SetOperation_Function, new Connector);
 
 void ResizeOperation_Function()
 {
     cout << "Choose Sequence to Resize.\n";
-    cout << "From 1 to " << Sequences.GetSize();
+    cout << "From 1 to " << Sequences.GetSize() << "\n";
     int number, newSize;
     cin >> number;
     cout << "Input new size.\n";
     cin >> newSize;
-    Sequences.Get(number)->Resize(newSize);
+    Sequences.Get(number-1)->Resize(newSize);
     cout << "Ready!\n";
 }
-Shell* SetOperation_Function = new Shell("Operation Resize", ResizeOperation_Function, new Connector);
 
 void InsertOperation_Function()
 {
     cout << "Choose Sequence to Insert.\n";
-    cout << "From 1 to " << Sequences.GetSize();
+    cout << "From 1 to " << Sequences.GetSize() << "\n";
     int number, index, item;
     cin >> number;
     cout << "Input index to Set.\n";
     cin >> index;
     cout << "Input item to Set.\n";
     cin >> item;
-    Sequences.Get(number)->Insert(index, item);
+    Sequences.Get(number-1)->Insert(index, item);
     cout << "Ready!\n";
 }
-Shell* SetOperation_Function = new Shell("Operation Insert", InsertOperation_Function, new Connector);
 
 void AppendOperation_Function()
 {
     cout << "Choose Sequence to Append.\n";
-    cout << "From 1 to " << Sequences.GetSize();
+    cout << "From 1 to " << Sequences.GetSize() << "\n";
     int number, item;
     cin >> number;
     cout << "Input item to Append.\n";
     cin >> item;
-    Sequences.Get(number)->Append(item);
+    Sequences.Get(number-1)->Append(item);
     cout << "Ready!\n";
 }
-Shell* SetOperation_Function = new Shell("Operation Append", AppendOperation_Function, new Connector);
 
-void ConcatOperation_Function()
+void PrependOperation_Function()
 {
-    cout << "Choose first Sequence to Concat.\n";
-    cout << "From 1 to " << Sequences.GetSize();
-    int number1, number2;
-    cin >> number1;
-    cout << "Choose second Sequence to Concat.\n";
-    cin >> number2;
-
-    ArraySequence<int> seq(*(Sequences.Get(number2)));
-    
-
-    if (Sequences.Get(number2)->GetSequenceType() == Array)
-        Sequences.Append(Sequences.Get(number1)->Concat(seq));
+    cout << "Choose Sequence to Prepend.\n";
+    cout << "From 1 to " << Sequences.GetSize() << "\n";
+    int number, item;
+    cin >> number;
+    cout << "Input item to Prepend.\n";
+    cin >> item;
+    Sequences.Get(number-1)->Prepend(item);
     cout << "Ready!\n";
 }
-Shell* SetOperation_Function = new Shell("Operation Append", AppendOperation_Function, new Connector);
+ 
+void Test_Function()
+{
+    ALLTESTS_SEQUENCE();
+}
 
-
-//        virtual Sequence<T>*    Concat(ArraySequence<T> &)  = 0;
-
-
-
-Shell* Exit = new Shell("Exit Window", Exit_Function, new Connector);
 void Exit_Function()
 {
     cout << "Confirm exit from the program. Type EXIT\n";
@@ -172,10 +171,53 @@ void Exit_Function()
     }
 }
 
-
-
 int main()
 {
-    // Head->Get_Connector()->Append_Shell();
-    // Head->Get_Connector()->Append_Shell();
+    Head =               new Shell("Head Window",         Head_Function, new Connector);
+    CreateSequence =     new Shell("Create Sequence",     CreateSequence_Function, new Connector);
+    ListOfSequences =    new Shell("List of Sequences",   ListOfSequences_Function, new Connector);
+    SequenceOperations = new Shell("Sequence Operations", nullptr, new Connector);
+    SetOperation =       new Shell("Set",                 SetOperation_Function, new Connector);
+    ResizeOperation =    new Shell("Resize",              ResizeOperation_Function, new Connector);
+    InsertOperation =    new Shell("Insert",              InsertOperation_Function, new Connector);
+    AppendOperation =    new Shell("Append",              AppendOperation_Function, new Connector);
+    PrependOperation =   new Shell("Prepend",             PrependOperation_Function, new Connector);
+    Test =               new Shell("Test",                Test_Function, new Connector);
+    Exit =               new Shell("Exit",                Exit_Function, new Connector);
+
+    Head->Get_Connector()->Append_Shell(CreateSequence);
+    Head->Get_Connector()->Append_Shell(Test);
+    Head->Get_Connector()->Append_Shell(Exit);
+    
+    CreateSequence->Get_Connector()->Append_Shell(Head);
+
+    ListOfSequences->Get_Connector()->Append_Shell(Head);
+
+    SequenceOperations->Get_Connector()->Append_Shell(SetOperation);
+    SequenceOperations->Get_Connector()->Append_Shell(ResizeOperation);
+    SequenceOperations->Get_Connector()->Append_Shell(InsertOperation);
+    SequenceOperations->Get_Connector()->Append_Shell(AppendOperation);
+    SequenceOperations->Get_Connector()->Append_Shell(PrependOperation);
+    SequenceOperations->Get_Connector()->Append_Shell(Head);
+
+    SetOperation->Get_Connector()->Append_Shell(SetOperation);
+    SetOperation->Get_Connector()->Append_Shell(Head);
+
+    ResizeOperation->Get_Connector()->Append_Shell(Head);
+
+    InsertOperation->Get_Connector()->Append_Shell(InsertOperation);
+    InsertOperation->Get_Connector()->Append_Shell(Head);
+
+    AppendOperation->Get_Connector()->Append_Shell(AppendOperation);
+    AppendOperation->Get_Connector()->Append_Shell(Head);
+    
+    PrependOperation->Get_Connector()->Append_Shell(PrependOperation);
+    PrependOperation->Get_Connector()->Append_Shell(Head);
+    
+    Test->Get_Connector()->Append_Shell(Head);
+
+    Exit->Get_Connector()->Append_Shell(Head);
+
+    ActiveShell Active(Head);
+    Active.StartUI();
 }
